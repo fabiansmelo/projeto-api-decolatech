b48029292f346208f063d929e51030d1534bd424
fabiansmelo <fabiangiovanni3@gmail.com>
2025-03-26 16:18:38 -0300

    Imprementacao da camada de dominio

--- src/main/java/dio/projetoapi/SantanderDevWeek2023Application.java
+++ src/main/java/dio/projetoapi/SantanderDevWeek2023Application.java
@@ -1,4 +1,4 @@
-package dio.projetoapi.santander_dev_week_2023;
+package dio.projetoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
