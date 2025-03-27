eeda1c8b65320b593ce2df692ec19fec6956bb73
fabiansmelo <fabiangiovanni3@gmail.com>
2025-03-27 00:47:19 -0300

    Added Controllers and Exceptions

---
+++ src/main/java/dio/projetoapi/services/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package dio.projetoapi.services.impl;
+
+import dio.projetoapi.domain.model.User;
+import dio.projetoapi.services.UserService;
+import org.springframework.stereotype.Service;
+import dio.projetoapi.domain.repository.UserRepository;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
